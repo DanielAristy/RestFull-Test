ed0727e570f047acd42977a3364c3ec5629b0a3e
DanielAristy <danielaristy22@gmail.com>
2021-06-30 08:37:20 -0500

    Se realiza test de peticion get para obtener los widgets esperados

---
+++ src/test/java/com/example/RESTfulTest/controller/WidgetRestControllerTest.java
@@ -0,0 +1,64 @@
+package com.example.RESTfulTest.controller;
+
+import com.example.RESTfulTest.model.Widget;
+import com.example.RESTfulTest.service.WidgetService;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WidgetRestControllerTest {
+
+    @MockBean
+    private WidgetService service;
+
+    //Se crea una instancia
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getExitoso() throws Exception {
+        //Arrange
+        Widget widget1 = new Widget(1l, "Widget Name", "Description", 1);
+        Widget widget2 = new Widget(2l, "Widget 2 Name", "Description 2", 4);
+
+        //Crea una lista sin constructor
+        doReturn(Lists.newArrayList(widget1, widget2)).when(service).findAll();
+
+        //Act && Assert
+        mockMvc.perform(get("/rest/widgets"))
+                // Validate the response code and content type
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                // Validate headers
+                .andExpect(header().string(HttpHeaders.LOCATION, "/rest/widgets"))
+
+                // Validate the returned fields
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("Widget Name")))
+                .andExpect(jsonPath("$[0].description", is("Description")))
+                .andExpect(jsonPath("$[0].version", is(1)))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].name", is("Widget 2 Name")))
+                .andExpect(jsonPath("$[1].description", is("Description 2")))
+                .andExpect(jsonPath("$[1].version", is(4)));
+    }
+
+}
